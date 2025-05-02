5f38a973f5763d7bda7b34e1d2f51ab0d532b587
Talal-Hassan-Programmer <Talal-Hassan@outlook.com>
2025-05-02 19:43:14 +0300

    Creating DropDown Menus in MenuBar

--- App.py
+++ App.py
@@ -25,6 +25,45 @@ MenuBar = App.menu = Menu(App)
 App.config(menu=MenuBar)
 
 
+# Creaeting submenu for app
+#------------------------------------------------------------------------------------------------------------------------------------------------------------->
+
+
+#1 - MenuBar - File Menu --------------------------------------------------------------------------->
+
+FileMenu = Menu(MenuBar, tearoff=0)
+MenuBar.add_cascade(label="File", menu=FileMenu)
+
+#Commands for File Menu
+
+
+
+#2 - MenuBar - Edit Menu --------------------------------------------------------------------------->
+
+EditMenu = Menu(MenuBar, tearoff=0)
+MenuBar.add_cascade(label="Edit", menu=EditMenu)
+
+#Commands for Edit Menu
+
+
+
+#3 - MenuBar - Effect Menu   --------------------------------------------------------------------------->
+
+EffectMenu = Menu(MenuBar, tearoff=0)
+MenuBar.add_cascade(label="Effects", menu=EffectMenu)
+
+#Commands for Effect Menu
+
+
+
+#4 - MenuBar - Enhance Menu   --------------------------------------------------------------------------->
+
+EnhanceMenu = Menu(MenuBar, tearoff=0)
+MenuBar.add_cascade(label="Enhance", menu=EnhanceMenu)
+
+#Commands for Effect Menu
+
+
 
 
 
